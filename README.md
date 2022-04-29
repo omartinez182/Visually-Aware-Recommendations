44e7f5232458d82ed6d4c5daad5ef14a3461bb80
omartinez@domalabs.com <omartinez@domalabs.com>
2022-04-29 00:32:56 -0400

    Updates to README.md

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 # Visually-Aware-Recommendations
  
 
-In this repository we try to experiment and provide some benchmarks for different recommendation algorithms, mainly we are interested in benchmarking the [VBPR](https://arxiv.org/pdf/1510.01784.pdf) & [CausalRec](https://arxiv.org/pdf/2107.02390.pdf), and compared their performance with other methods such as MM-MF or the most popular approach.
+In this repository we try to experiment and provide some benchmarks for different recommendation algorithms, mainly we are interested in benchmarking the [VBPR](https://arxiv.org/pdf/1510.01784.pdf) & [CausalRec](https://arxiv.org/pdf/2107.02390.pdf), and compared their performance with the BPR, MM-MF, and MostPop recommenders.
 
 We use two datasets to do this, the [Tradesy](http://jmcauley.ucsd.edu/data/tradesy/) and the [Amazon Clothing](https://nijianmo.github.io/amazon/index.html) datasets, the latter can be segmented across different categories.
 
@@ -9,15 +9,28 @@ For the modeling and evaluation we use the [Cornac Framework](https://cornac.pre
 
 ## Getting Started
 
-To run the code locally, first clone this repository and install the requirements.
+1) To run the code locally, first clone this repository and install the requirements.
 
 ```bash
 git clone https://github.com/omartinez182/Visually-Aware-Recommendations.git && cd Visually-Aware-Recommendations
 pip install -r requirements.txt
 ```
+2) Then you can proceed to run the experiment with:
 
+```bash
+$ python3 run_experiment.py
+```
+The defualt dataset is the Amazon Clothing, but you can pass the dataset as an argument, so if you'd like to run the experiment with the Tradesy dataset you can use the following:
+
+```bash
+$ python3 run_experiment.py --dataset tradesy
+```
 
 ## Results
+
+The results of the experiment are saved as a log file under:
+
+```data``` -> ```output``` -> ```model_eval```.
 
 **Tradesy VBPR Experiment Results**
 
---
+++ run_experiment.py
@@ -0,0 +1,59 @@
+import argparse
+import cornac
+from cornac.datasets import amazon_clothing
+from cornac.datasets import tradesy
+from cornac.data import ImageModality
+from cornac.eval_methods import RatioSplit
+from cornac.models import MostPop, MMMF, BPR, VBPR, CausalRec
+
+
+def main(args):
+    """
+    Experiment to compare the performance of the MostPop, MMMF, 
+    BPR, VBPR, and CausalRec recommenders on either the Tradesy or
+    the Amazon Clothing datasets.
+    """
+    SEED = 42
+    VERBOSE = True
+
+    # Load the Amazon data
+    feedback = args.dataset.load_feedback()
+    features, item_ids = args.dataset.load_visual_feature()
+    
+    # Visual features
+    item_image_modality = ImageModality(features=features, ids=item_ids, normalized=True)
+
+    # Evaluation and train/test splits.
+    ratio_split = RatioSplit(
+        data=feedback,
+        test_size=0.1, # Test size is not reported on the VBPR paper.
+        rating_threshold=0.5,
+        exclude_unknowns=True,
+        verbose=VERBOSE,
+        item_image=item_image_modality,
+        seed = SEED,
+        )
+
+    # Instantiate models using similar parameters to the VBPR paper.
+    models = [MostPop(),
+          MMMF(k=10, learning_rate=1, seed=SEED), # k are the latent factors 
+          BPR(k=10, learning_rate=0.1, seed=SEED),
+          VBPR(k=10, k2=10, learning_rate=0.1, seed=SEED, use_gpu=True),
+          CausalRec(k=10, k2=10, n_epochs=1, learning_rate=0.01,
+                    mean_feat=features.mean(axis=0), tanh=1, use_gpu=True, seed=SEED)
+          ]
+
+    # Instantiate evaluation metric
+    auc = cornac.metrics.AUC()
+
+    # Put everything together into an experiment and run it and save the results as a log file.
+    cornac.Experiment(eval_method=ratio_split, models=models, metrics=[auc], save_dir='data/output/model_eval').run()
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--dataset',
+                        type=str,
+                        default=amazon_clothing)
+    args = parser.parse_args()
+    main(args)
